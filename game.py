218c22b3db6b457c5660b29b38e08ac6a14a0548
Jennifer Lei <jenniferlei1928@gmail.com>
2021-10-16 19:20:04 +0000

    Added play game loop

--- game.py
+++ game.py
@@ -19,19 +19,19 @@ player_name = input("What's your name? ")
 print(f"{player_name}, I'm thinking of a number between 1 and 100.")
 print("Try to guess my number.")
 
-# num = random.randint(1, 100)
+num = random.randint(1, 100)
 
-# play_game = True
+play_game = True
 
-# while play_game:
-#     tries = 0
-#     guess = int(input("Your guess? "))
-#     if guess == num:
-#         print(f"Well done, {player_name}! You found my number in {tries} tries!")
-#         play_game = False
-#     elif guess > num:
-#         print("Your guess is too high, try again.")
-#         tries += 1
-#     elif guess < num:
-#         print("Your guess is too low, try again.")
-#         tries += 1
+while play_game:
+    tries = 0
+    guess = int(input("Your guess? "))
+    if guess == num:
+        print(f"Well done, {player_name}! You found my number in {tries} tries!")
+        play_game = False
+    elif guess > num:
+        print("Your guess is too high, try again.")
+        tries += 1
+    elif guess < num:
+        print("Your guess is too low, try again.")
+        tries += 1
