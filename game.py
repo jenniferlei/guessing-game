623ed5135e2256f776a8d1bd0faa02f4a4464399
Jennifer Lei <jenniferlei1928@gmail.com>
2021-10-16 19:46:32 +0000

    Added guess function to ensure input is valid

--- game.py
+++ game.py
@@ -13,6 +13,21 @@
 
 import random
 
+def guess_input():
+    '''determines if user input a valid number equal or between 1 and 100.
+    returns the guess if valid'''
+    while True:
+        guess = input("Your guess? ")
+
+        if guess.isnumeric():
+            guess = int(guess)
+            if guess > 0 and guess < 101:
+                return guess
+
+        print("How dare you. That is an invalid input. Please try again.")
+
+
+
 
 print("Welcome to the guessing game!")
 player_name = input("What's your name? ")
@@ -22,11 +37,13 @@ print("Try to guess my number.")
 num = random.randint(1, 100)
 
 play_game = True
+tries = 0
+
 
 while play_game:
-    tries = 0
-    guess = int(input("Your guess? "))
+    guess = guess_input()
     if guess == num:
+        tries += 1
         print(f"Well done, {player_name}! You found my number in {tries} tries!")
         play_game = False
     elif guess > num:
@@ -35,3 +52,6 @@ while play_game:
     elif guess < num:
         print("Your guess is too low, try again.")
         tries += 1
+    
+
+
